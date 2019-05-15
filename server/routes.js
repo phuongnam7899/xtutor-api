2ee302b52bc534b9551138be0fdc61907b78d29a
phuongnam7899 <phuongnam7899@gmail.com>
2019-05-15 21:10:35 +0700

    stupid login

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,11 @@
 import examplesRouter from './api/controllers/examples/router';
 import authRouter from './api/controllers/auth/router';
 const bodyParser = require("../node_modules/body-parser");
+const morgan = require('../node_modules/morgan');
 
 export default function routes(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(morgan('dev'));
   app.use('/api/examples', examplesRouter);
   app.use("/api/auth",authRouter);
 }
